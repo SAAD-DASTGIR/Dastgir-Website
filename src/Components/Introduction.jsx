74d2054b14e899c2f49dacbaa9815d70916bc623
SAAD-DASTGIR <msaaddastgir@gmail.com>
2024-04-27 18:32:50 +0500

    ADDED Introduction,navbar,about me boiler plate

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Introduction =()=>{
+
+    return(
+        <>
+            test introduction
+        </>
+
+    )
+}
+
+export default Introduction
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar=()=>{
+    return(
+        <>
+            <>test navbar</>
+        </>
+        
+    )
+}
+export default Navbar
